27c849767d68c8f6d84191da9761a9788367f7a7
Emanuele213 <vittorius213@gmail.com>
2022-10-26 11:15:21 +0200

    bug fix

--- Dati/js/script.js
+++ Dati/js/script.js
@@ -1,25 +1,28 @@
 const eleInizia = document.querySelector('.invia'); 
 let outUserNum = document.querySelector('.num-user');
 let outCpuNumer = document.querySelector('.num-cpu');
-let winUser = document.querySelector('.win-user');
-let winCpu = document.querySelector('.win-cpu');
-let winBoth = document.querySelector('.win-both');
+let win = document.querySelector('.win');
 let ricomincia = document.querySelector('.reload');
 
+let userNum;
+let cpuNum
+
 eleInizia.addEventListener('click', function () {
-let userNum = Math.floor(Math.random() * 6 ) + 1;
+userNum = Math.floor(Math.random() * 6 ) + 1;
 console.log('Il numero della cpu e\'', userNum);
 outUserNum.innerHTML = (`Il tuo numero e\' ${userNum}`);
-let cpuNum = Math.floor(Math.random() * 6 ) + 1;
+cpuNum = Math.floor(Math.random() * 6 ) + 1;
 console.log('Il numero della cpu e\'', cpuNum);
 outCpuNumer.innerHTML = (`Il numero della CPU e\' ${cpuNum}`);
 
 if (userNum == cpuNum) {
-    winBoth.innerHTML = ('Avete vinto entrambi')
-}else if (userNum < cpuNum) {
-    winCpu.innerHTML = ('Ha vinto la CPU')
-}else {
-    winUser.innerHTML = ('Hai vinto')
+    win.innerHTML = ('Avete vinto entrambi');
+}
+if (userNum < cpuNum) {
+    win.innerHTML = ('Ha vinto la CPU');
+}
+if (userNum > cpuNum){
+    win.innerHTML = ('Hai vinto');
 }
 });
 
